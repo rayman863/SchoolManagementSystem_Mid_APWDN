5fb5503766d7b8c8fefe8ef5bc10ac9fce006f29
ShihabShahib <shahibshihab@gmail.com>
2020-08-07 03:49:43 +0600

    all 17 extend modelclass

---
+++ Mid_SchoolManagementSystem/Mid_SchoolManagementSystem/Models/Extended/grade.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mid_SchoolManagementSystem.Models.DataAccess
+{
+    [MetadataType(typeof(admindata))]
+    public partial class grade
+    {
+    }
+
+    public class gradedata
+    {
+        [Display(Name = "Grade Id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Grade Id is required")]
+        public int gradeid { get; set; }
+
+        [Display(Name = "Quiz 1 mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Quiz 2 mark is required")]
+        public int quiz1 { get; set; }
+
+        [Display(Name = "Quiz 2 mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Quiz 2 mark is required")]
+        public int quiz2 { get; set; }
+
+        [Display(Name = "Assignment mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Assignment mark is required")]
+        public int assignment1 { get; set; }
+
+        [Display(Name = "Half Yearly Grade")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Half Yearly Grade is required")]
+        public int halfyearlygrade { get; set; }
+
+        [Display(Name = "Quiz 3 mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Quiz 3 mark is required")]
+        public int quiz3 { get; set; }
+
+        [Display(Name = "Quiz 4 mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Quiz 4 mark is required")]
+        public int quiz4 { get; set; }
+
+        [Display(Name = "Assignment 2 mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Assignment 2 mark is required")]
+        public int assignment2 { get; set; }
+
+        [Display(Name = "Final Grade mark")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Final Grade is required")]
+        public int finalexamgrade { get; set; }
+    }
+}
--- Mid_SchoolManagementSystem/Mid_SchoolManagementSystem/Models/Extended/student.cs
+++ Mid_SchoolManagementSystem/Mid_SchoolManagementSystem/Models/Extended/student.cs
@@ -18,28 +18,59 @@ namespace Mid_SchoolManagementSystem.Models.DataAccess
         [Display(Name = "Student ID")]
         //[Required(AllowEmptyStrings = false, ErrorMessage = "Super Admin ID is required")]
         public string studentid { get; set; }
+
+        //stname
         [Display(Name = "Student Name")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Student Name is required")]
         public string studentname { get; set; }
+
+        //stpass
         [Display(Name = "Student Password")]
         [DataType(DataType.Password)]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Student Password is required")]
         [MinLength(8, ErrorMessage = "Atleast 8 characters required")]
         public string studentpassword { get; set; }
+        
+        //stdob
         [Display(Name = "Student Date Of Birth")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Birth Date is required")]
         [DataType(DataType.Date)]
         public System.DateTime studentdob { get; set; }
+        
+        //stphone
         [Display(Name = "Student Phone")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Phone Number is required")]
         public int studentphone { get; set; }
+        
+        //staddress
         [Display(Name = "Student Address")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Student Address is required")]
         public string studentaddress { get; set; }
+        
+        //stemail
+        [Display(Name = "Student Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Student Email is required")]
+        [DataType(DataType.EmailAddress)]
         public string studentemail { get; set; }
-        public string studentbloodgroup { get; set; }
+        
+        //stbloodgroup
+        [Display(Name = "Student bloodgroup")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Blood group need to be mentioned")]
+       public string studentbloodgroup { get; set; }
+
+        //stfees
+        [Display(Name = "Student Fees")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fees amount required")]
         public int studentfees { get; set; }
+
+        //classid
+        [Display(Name = "Class id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "class id required")]
         public int classid { get; set; }
+
+        //sectionid
+        [Display(Name = "Section id")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "section id required")]
         public int sectionid { get; set; }
     }
 }
