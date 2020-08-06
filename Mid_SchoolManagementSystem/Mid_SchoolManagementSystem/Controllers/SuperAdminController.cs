bc05830c89cdd0399d584390099d5cfb8dc4d910
Raiyan Islam <raiyanislam863@gmail.com>
2020-08-06 19:39:01 +0600

    Super Admin Create

---
+++ Mid_SchoolManagementSystem/Mid_SchoolManagementSystem/Controllers/SuperAdminController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mid_SchoolManagementSystem.Models.DataAccess;
+
+
+namespace Mid_SchoolManagementSystem.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        //SuperAdmin Create GET
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //SuperAdmin Create POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "id")] superadmin superadmin)
+        {
+            bool Status = false;
+            string message = "";
+
+            superadmin.superadminid = generateSuperID();//"20-0005-01";
+
+            if (ModelState.IsValid)
+            {
+                var nameExists = NameExists(superadmin.superadminname);
+                if (nameExists)
+                {
+                    ModelState.AddModelError("NameExist", "Super Admin name already exists");
+                    return View(superadmin);
+                }
+
+                superadmin.superadminpassword = Crypto.Hash(superadmin.superadminpassword);
+                superadmin.superadminconfirmpassword = Crypto.Hash(superadmin.superadminconfirmpassword);
+
+                using (smsEntities data = new smsEntities())
+                {
+                    data.superadmin.Add(superadmin);
+                    data.SaveChanges();
+                    message = " Super Admin Account " + superadmin.superadminname + " with ID = " + superadmin.superadminid + " has been created.";
+                    Status = true;
+                }
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(superadmin);
+        }
+
+        [NonAction]
+        public bool NameExists(string superadminname)
+        {
+            using (smsEntities data = new smsEntities())
+            {
+                var name = data.superadmin.Where(a => a.superadminname == superadminname).FirstOrDefault();
+                return name != null;
+            }
+        }
+
+        [NonAction]
+        public string generateSuperID()
+        {
+            using (smsEntities data = new smsEntities())
+            {
+                var oldID = (from superadmin in data.superadmin
+                             orderby
+                             superadmin.id descending
+                             select superadmin.superadminid).Take(1).FirstOrDefault();//.ToString();
+                //Debug.WriteLine(oldID);
+                string toBreak = oldID.ToString();
+                string[] idList = toBreak.Split('-');//20-0000-01
+                //foreach (string id in idList)
+                //{ Debug.WriteLine(id); }
+                string id1 = idList[0];
+                //Debug.WriteLine(id1);
+                string id2 = idList[1];
+                //Debug.WriteLine(id2);
+                string id3 = idList[2];
+                //Debug.WriteLine(id3);
+                int idInc = Convert.ToInt32(id2);
+                idInc = idInc + 1;
+                id2 = idInc.ToString("D" + 4);
+                string newID = id1 + "-" + id2 + "-" + id3;
+                return newID;
+            }
+        }
+
+
+    }
+}
